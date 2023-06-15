641b011d91ada912a72781508fbf086e998dc3dd
Tilek <Tilek@users.noreply.github.com>
2023-06-15 11:45:21 +0600

    Create toolbar, connect Mobx

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Canvas.scss'
+import { observer } from "mobx-react-lite";
 
-export const Canvas = () => {
+export const Canvas = observer( () => {
   return (
     <div className='canvas'>
         <canvas width={600} height={400}/>
     </div>
   )
-}
+});
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import "../styles/Toolbar.scss"
+import React from "react";
+import "../styles/Toolbar.scss";
 
 export const Toolbar = () => {
   return (
-    <div className='toolbar'>Toolbar</div>
-  )
-}
+    <div className="toolbar">
+      <button className="toolbar__btn brush"></button>
+      <button className="toolbar__btn rect"></button>
+      <button className="toolbar__btn circle"></button>
+      <button className="toolbar__btn eraser"></button>
+      <button className="toolbar__btn line"></button>
+      <input style={{marginLeft: 10}} type="color" />
+      <button className="toolbar__btn undo"></button>
+      <button className="toolbar__btn redo"></button>
+      <button className="toolbar__btn save"></button>
+    </div>
+  );
+};
