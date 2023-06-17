4097fa1de4ec4ac6de124e6103cc8f7a65630db7
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-06-18 00:00:00 +0600

    Create undo and Redo function

--- src/store/canvasState.js
+++ src/store/canvasState.js
@@ -2,12 +2,41 @@ import { makeAutoObservable } from "mobx";
 
 class CanvasState {
   canvas = null;
+
+  undoList = []
+  redoList = []
+
   constructor() {
     makeAutoObservable(this);
   }
   setCanvas(canvas) {
     this.canvas = canvas;
   }
+  pushToUndo(data) {
+  this.undoList.push(data)
+  }
+  pushToRedo (data) {
+ this.redoList.push(data)
+  }
+
+undo() {
+let ctx = this.canvas.getContext('2d');
+if(this.undoList.length > 0) {
+   let dataUrl = this.undoList.pop()
+   let img = new Image()
+   let src = dataUrl
+   img.onload = () => {
+    ctx.clearReact(0, 0, this.canvas.width, this.canvas.height)
+   }
+}else {
+  ctx.drawImage(img , 0, 0, this.canvas.widht, this.canvas.height)
+}
+}
+
+redo() {
+
+}
+
 }
 
 export default new CanvasState();
